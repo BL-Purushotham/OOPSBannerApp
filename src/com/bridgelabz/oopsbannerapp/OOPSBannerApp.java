1f4d147ff11c565800073243c69890605cdea8c0
BL-Purushotham <purushotham.cirimla@bridgelabz.com>
2026-01-05 11:30:42 +0530

    UC1: Print OOPS to Console

---
+++ src/com/bridgelabz/oopsbannerapp/OOPSBannerApp.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.oopsbannerapp;
+
+
+
+import java.util.Arrays;
+
+public class OOPSBannerApp {
+   
+	   public static void main(String[] args) {
+	    	
+	        String text = "OOPS";
+	        System.out.println(text);
+	    }
+}
+  
